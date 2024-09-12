f99a6d622b2365cd0b189efdd75e16557b7bcde6
Shahd El-Badrawy <shahdbadrawy@gmail.com>
2024-09-12 17:29:05 +0300

    Add database context and Sates model

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace InstaHub.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Models/Sates.cs
@@ -0,0 +1,9 @@
+﻿namespace InstaHub.Models
+{
+    public enum Sates
+    {
+        open = 1,
+        closed = 2,
+        inprogress = 3
+    }
+}
