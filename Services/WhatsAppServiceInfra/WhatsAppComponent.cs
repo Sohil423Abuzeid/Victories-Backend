eb50424a2e35c24b4e8a422f365b289437c7c34d
Shahd El-Badrawy <shahdbadrawy@gmail.com>
2024-09-19 17:11:05 +0300

    Refactor: Remove redundant WhatsApp service components for streamlined integration

--- Services/WhatsAppServiceInfra/WhatsAppComponent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InstaHub.Services.WhatsAppServiceInfra
-{
-    public class WhatsAppComponent
-    {
-        public string type { get; set; }
-        public List<object> parameters { get; set; }
-    }
-}
